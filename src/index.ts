acacb3a3dd680aad7ebddbf3981922e742097c85
kari0003 <karesz@codingsans.com>
2021-05-25 08:45:52 +0200

    add gql to the project

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,26 @@ import { createSocketServer } from './app';
 import { config } from './config';
 import express from 'express';
 import cors from 'cors';
+import { ApolloServer } from 'apollo-server-express';
+import resolvers from './platform/graphql/resolvers';
+import typedefs from './platform/graphql/typedefs';
 
 const INDEX = '/index.html';
+const throwGlobal = (err: Error) => {
+  throw err;
+};
 
-export const main = () => {
-  const httpServer = express()
+export const main = async (): Promise<void> => {
+  const app = express();
+  const apolloServer = new ApolloServer({
+    typeDefs: typedefs,
+    resolvers: resolvers,
+  });
+
+  await apolloServer.start();
+  apolloServer.applyMiddleware({ app });
+
+  const httpServer = app
     .use(cors({ origin: '*', methods: ['GET', 'POST'] }))
     .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
     .use('/status', (req, res) => res.send({ status: 'ok' }))
@@ -17,5 +32,5 @@ export const main = () => {
 
 // Run main if its the entry point
 if (!module.parent) {
-  main();
+  main().catch((err) => throwGlobal(err));
 }
